edf563f4596a9a9d29d764e62a41dc6177094831
theddy9098 <teddyfso15@gmail.com>
2025-08-25 17:24:00 -0500

    Primer commit de los arreglos

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+int [] numeros = new int[3];
+
+numeros[0] = 1;
+numeros[1] = 2;
+numeros[2] = 3;
+
+    }
+}
