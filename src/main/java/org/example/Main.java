3133c0c14a7873475cc2638f156ccede33bc7304
theddy9098 <teddyfso15@gmail.com>
2025-08-27 18:40:31 -0500

    Recorrido del arreglo usando el bucle for-each clacico

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,14 @@ public class Main {
 int [] numeros = new int[10];
 Random aleatorio = new Random();
 
-for (int i = 0; i < 10; i++) {
-    numeros[i] = aleatorio.nextInt(100) + 1;
-}
-        System.out.println("Arreglo iniciado con numeros aleatorios");
-for (int i = 0; i < 10; i++) {
-    System.out.println(numeros[i]);
+ for(int i=0;i<numeros.length;i++){
+     numeros[i]=aleatorio.nextInt(100);
+ }
+
+    System.out.println("muestra los numeros en pantalla");
+    for (int i:numeros) {
+    System.out.println(i);
+            
 }
 
     }
