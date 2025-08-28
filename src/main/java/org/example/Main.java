22eead54e7021b182950142bcdc2aa620d7b24da
theddy9098 <teddyfso15@gmail.com>
2025-08-28 00:15:34 -0500

    Suma de toda la matriz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,23 @@ public class Main {
     public static void main(String[] args) {
         int[][] Matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-        for (int j = 0; j < 3; j++) {   //con esto recorremos la matriz en forma de colomnas
-            for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(Matriz[i][j] + " ");
             }
-
-            // Aqui imprimimos en forma de tabla
             System.out.println();
+        }
+
+
+        int suma = 0;
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j < 3; j++) {
+                suma += Matriz[i][j] ;
+
+            }
 
 
         }
+        System.out.println("El resultado de la suma de la matriz es: " + suma);
     }
 }
