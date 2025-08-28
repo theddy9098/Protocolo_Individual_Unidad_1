843defd5ee3d3ff59cb61c9b2b7f8daf4e8453aa
theddy9098 <teddyfso15@gmail.com>
2025-08-28 15:54:42 -0500

    Ejercicio individual Del protocolo individual

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,44 +2,72 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] Matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[] Arreglo = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(Matriz[i][j] + " ");
-            }
-            System.out.println();
+        boolean asc = true;
+        boolean desc = true;
+
+        for (int i = 0; i < Arreglo.length; i++) {
+            System.out.print(Arreglo[i] + " ");
         }
 
-        int suma = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                suma += Matriz[i][j];
+        System.out.println(" ");
+
+        for (int i=0; i<Arreglo.length-1; i++) {
+            if (Arreglo[i] < Arreglo[i + 1]) {
+                desc = false;
+            } else if (Arreglo[i] > Arreglo[i + 1]) {
+                asc = false;
 
             }
-
-
+        }
+        if (asc) {
+            System.out.println("El arreglo está en orden ascendente.");
+        } else if (desc) {
+            System.out.println("El arreglo está en orden descendente.");
+        } else {
+            System.out.println("El arreglo está desordenado.");
         }
 
-        System.out.println("El resultado de la suma de la matriz es: " + suma);
-
-        //Variable donde se almacena la primera fila de la matriz y donde se remplaza por la ultima
-        int[] temp = Matriz[0];
-        Matriz[0] = Matriz[Matriz.length - 1];
-        Matriz[Matriz.length - 1] = temp;
-
-        System.out.println("Matriz después de intercambiar la primera fila con la última:");
-        imprimirMatriz(Matriz);
-
-    }
-     // Metodo necesario para imprimir la matriz cambiada
-    public static void imprimirMatriz(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + "  ");
-            }
-            System.out.println();
+        if (esAritmetica(Arreglo)) {
+            System.out.println("El arreglo forma una progresión aritmética.");
+        } else if (esGeometrica(Arreglo)) {
+            System.out.println("El arreglo forma una progresión geométrica.");
+        } else {
+            System.out.println("El arreglo no forma ninguna progresión.");
         }
     }
 
+    static boolean esAritmetica(int[] Arreglo) {
+        if (Arreglo.length < 2) return false;
+        int diferencia = Arreglo[1] - Arreglo[0];
+
+        for (int i = 1; i < Arreglo.length - 1; i++) {
+            if (Arreglo[i + 1] - Arreglo[i] != diferencia) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean esGeometrica(int[] Arreglo) {
+        if (Arreglo.length < 2) return false;
+        if (Arreglo[0] == 0) return false; // evitar división por cero
+        double razon = (double) Arreglo[1] / Arreglo[0];
+
+        for (int i = 1; i < Arreglo.length - 1; i++) {
+            if (Arreglo[i] == 0 || (double) Arreglo[i + 1] / Arreglo[i] != razon) {
+                return false;
+            }
+        }
+        return false;
+    }
 }
+
+
+
+
+
+
+
+
