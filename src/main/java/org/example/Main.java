f9724f42442dd1e73d27b72daf208b93e82e5d5f
theddy9098 <teddyfso15@gmail.com>
2025-08-28 00:29:40 -0500

    Intercambio de la primera fila de una matriz por la ultima

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,16 +11,35 @@ public class Main {
             System.out.println();
         }
 
-
         int suma = 0;
-        for (int i = 0; i <3 ; i++) {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                suma += Matriz[i][j] ;
+                suma += Matriz[i][j];
 
             }
 
 
         }
+
         System.out.println("El resultado de la suma de la matriz es: " + suma);
+
+        //Variable donde se almacena la primera fila de la matriz y donde se remplaza por la ultima
+        int[] temp = Matriz[0];
+        Matriz[0] = Matriz[Matriz.length - 1];
+        Matriz[Matriz.length - 1] = temp;
+
+        System.out.println("Matriz después de intercambiar la primera fila con la última:");
+        imprimirMatriz(Matriz);
+
     }
+     // Metodo necesario para imprimir la matriz cambiada
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
 }
