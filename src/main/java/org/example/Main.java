feb53820c2ce33d9295376845f0b858776a3782a
theddy9098 <teddyfso15@gmail.com>
2025-08-28 00:00:58 -0500

    Imorimir en forma de tabla y por columnas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,11 @@
 package org.example;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int Matriz[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] Matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-        for (int i = 0; i < 3; i++) {   //con esto recorremos la matriz en forma de filas
-            for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < 3; j++) {   //con esto recorremos la matriz en forma de colomnas
+            for (int i = 0; i < 3; i++) {
                 System.out.print(Matriz[i][j] + " ");
             }
 
